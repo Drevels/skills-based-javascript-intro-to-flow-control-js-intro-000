40ef6e888731a0e33c486d903a2e41bd23ffa9b7
danielle revels <ddaniellerrevels@aol.com>
2020-06-26 20:11:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,13 +12,13 @@ function teenager(age) {
 
 function ageChecker(age) {
   if(age >= 13 && age < 19){
-  return('You are a teenager!')}
-  {else if (age >19)
-  return('You are a grownup')}
-  {else (age < 12 )
-  return('You are a kid')}
+  return('You are a teenager!')
+  else if (age >19)
+  return('You are a grownup')
+    else (age < 12 )
+  return('You are a kid')
 }
-
+}
 function ternaryTeenager(age){
   ((age <= 13 || age <19 )?'You are not a teenager' : 'You are a teenager!')
 }
